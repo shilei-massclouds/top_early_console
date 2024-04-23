0093bacf1d45709fe8483b81eb1404f6dad4cde3
Shi Lei <shi_lei@massclouds.com>
2024-04-23 23:10:48 +0800

    panic: call arch_root::panic

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
 #![no_std]
 
-pub fn init(_cpu_id: usize, _dtb_pa: usize) {
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "Rust" fn runtime_main(_cpu_id: usize, _dtb_pa: usize) {
     let msg = "\n[early_console]: Hello, ArceOS!\n";
     early_console::write_bytes(msg.as_bytes());
+}
+
+pub fn panic(info: &PanicInfo) -> ! {
+    arch_boot::panic(info)
 }
